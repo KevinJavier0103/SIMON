096e8af3a15dd4293d8ddead2db7ad89e1ff3d67
Kevin Aguilar <kevinrock_0103@htomail.com>
2022-05-14 11:30:46 -0500

    Game complete

--- game.js
+++ game.js
@@ -1,10 +1,52 @@
+
 let buttonColors=["red","green","yellow","blue"];
 let gamePattern=[];
 let userClickParttern=[];
 let level=0;
 let start=false;
 
+
+$(document).keypress(function(){
+    if(!start){
+        $("h1").text("Level "+ level);
+        nextSequence();
+        start=true;
+    }
+});
+
+$("button").click(function(){
+    let userChoseColour= $(this).attr("id"); 
+    userClickParttern.push(userChoseColour); 
+    playSound(userChoseColour);
+    animatePress(userChoseColour);
+    checkAnswer(userClickParttern.length-1);
+})
+
+
+function checkAnswer(currenLevel){
+
+    if (gamePattern[currenLevel]=== userClickParttern[currenLevel]){
+        if (userClickParttern.length===gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+        }
+    } else {
+        let audio= new Audio("sounds/wrong.mp3");
+        audio.loop=false;
+        audio.play();
+        $("body").addClass("gameover");
+        $("h1").text("Game Over, Press Any Key to Restart");
+        setTimeout (function(){
+            $("body").removeClass("gameover");
+        },200);
+
+        startOver();
+    }
+}
+
 function nextSequence(){
+    userClickParttern=[]
     level++;
     $("h1").text("Level "+ level);
     let randomNumber=Math.floor(Math.random()*4);
@@ -12,44 +54,26 @@ function nextSequence(){
     gamePattern.push(randomChosenColor);
     $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColor);
-    animatePress(randomChosenColor);
-
     
-
 }
 
-$("button").click(function(event){
-    let userChoseColour=event.target.id; 
-    userClickParttern.push(userChoseColour); 
-    playSound(userChoseColour);
-    animatePress(userChoseColour);
-})
 
 function playSound(name){
     let audio =new Audio("sounds/"+name+".mp3");
     audio.loop=false;
     audio.play();
-
 }
 
-function animatePress (currentColour){
 
+function animatePress (currentColour){
     $("#"+currentColour).addClass("pressed");
-    
     setTimeout(function(){
         $("#"+currentColour).removeClass("pressed");
     },100);
-
 }
 
-$("body").keypress(function(){
-    if(!start){
-        $("h1").text("Level "+ level);
-        nextSequence();
-        start=true;
-    }
-    
-    
-
-
-});
+function statOver(){
+    level=0;
+    gamePattern=[];
+    start=false;
+}
